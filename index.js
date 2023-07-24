b62db076824422895a44bbb92b5c89746230fca7
ivandmitrienko <oneblackcrystal@gmail.com>
2023-07-24 17:25:09 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const itemRoutes = require("./routes/items");
+
 
 const app = express();
 const port = 3001;
 
 app.use(bodyParser.json());
 
-app.get("/", (req, res) => {
-  res.send("Hello World!!");
-});
+app.use("/items", itemRoutes);
 
 app.listen(port, () => {
   console.log(`Server started at port http://localhost:${port}`);
