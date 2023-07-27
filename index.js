5e926598e1b2b28348ee375030275c40eeff5478
ivandmitrienko <oneblackcrystal@gmail.com>
2023-07-27 21:55:08 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,15 +1,39 @@
+// const express = require("express");
+// const bodyParser = require("body-parser");
+// const itemRoutes = require("./routes/items");
+
+
+// const app = express();
+// const port = 3001;
+
+// app.use(bodyParser.json());
+
+// app.use("/items", itemRoutes);
+
+// app.use("/", (req, res) => {
+//   // res.json(itemData);
+//   res.sendStatus(408)
+// });
+
+// app.listen(port, () => {
+//   console.log(`Server started at port http://localhost:${port}`);
+// });
+
+
 const express = require("express");
-const bodyParser = require("body-parser");
-const itemRoutes = require("./routes/items");
-
-
+const fs = require("fs");
+    
 const app = express();
-const port = 3001;
-
-app.use(bodyParser.json());
-
-app.use("/items", itemRoutes);
-
-app.listen(port, () => {
-  console.log(`Server started at port http://localhost:${port}`);
+// const jsonParser = express.json();
+  
+app.use(express.static(__dirname + "/public"));
+  
+const filePath = "users.json";
+app.get("/api/users", function(req, res){
+       
+    const content = fs.readFileSync(filePath,"utf8");
+    const users = JSON.parse(content);
+    res.send(users);
 });
+ 
+app.listen(3001);
--- routes/items.js
+++ routes/items.js
@@ -1,23 +1,32 @@
-const express = require("express");
-const router = express.Router();
-const { v4 } = require("uuid");
+// const express = require("express");
+// const router = express.Router();
+// const { v4 } = require("uuid");
 
-let itemData = [
-   {
-      "name": "Waffles",
-      "toppings": "strawberry",
-      "price": 200
-   }
-];
+// let itemData = [
+//    {
+//       "name": "Waffles",
+//       "toppings": "strawberry",
+//       "price": 200
+//    }
+// ];
 
-router.get("/", (req, res) => {
-  res.json(itemData);
-});
+// // router.get("/", (req, res) => {
+// //   // res.json(itemData);
+// //   res.sendFile(__filename + "/README.md")
+// // });
 
-router.post("/", (req, res) => {
-  const item = req.body;
-  itemData.push({ ...item, id: v4() });
-  res.send(`Item "${item.name}" has been added successfully.`);
-});
+// router.post("/", (req, res) => {
+//   const item = req.body;
+//   itemData.push({ ...item, id: v4() });
+//   res.send(`Item "${item.name}" has been added successfully.`);
+// });
 
-module.exports = router;
+// router.get("/:id", (req, res) => {
+//   const { id } = req.params;
+//   console.log(id);
+//   const itemFound = itemData.find((item) => item.id === id);
+//   console.log(itemFound);
+//   res.send(itemFound);
+// });
+
+// module.exports = router;
