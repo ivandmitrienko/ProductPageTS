d327d478b665e05b425fcf209830c81a83adc830
ivandmitrienko <oneblackcrystal@gmail.com>
2023-08-01 22:23:42 +0300

    initial commit

---
+++ app.js
@@ -0,0 +1,20 @@
+const MongoClient = require("mongodb").MongoClient;
+   
+const url = "mongodb://127.0.0.1:27017/";
+const mongoClient = new MongoClient(url);
+ 
+async function run() {
+    try {
+        await mongoClient.connect();
+        const db = mongoClient.db("usersdb");
+        const collection = db.collection("users");
+        const result = await collection.updateOne({name: "Tom"}, { $set: {name: "Tom Junior", age:33}});
+        console.log(result);
+         
+    }catch(err) {
+        console.log(err);
+    } finally {
+        await mongoClient.close();
+    }
+}
+run().catch(console.error);
