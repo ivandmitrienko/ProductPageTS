d71f049d92d50ce018a9532f95140d66103306c5
ivandmitrienko <oneblackcrystal@gmail.com>
2023-08-29 17:17:49 +0300

    initial commit

--- src/components/store/index.ts
+++ src/components/store/index.ts
@@ -1,19 +1,7 @@
 import { createStore } from 'redux';
-import { persistStore, persistReducer } from 'redux-persist'
-import storage from 'redux-persist/lib/storage'
-// import { composeWithDevTools } from 'redux-devtools-extension';
 import { composeWithDevTools } from '@redux-devtools/extension';
 import reducers from '../reducers';
 
-const persistConfig = {
-    key: 'root',
-    storage,
-};
-
-const persistedReducer = persistReducer(persistConfig, reducers);
-
-const store = createStore(persistedReducer, composeWithDevTools());
-
-export const persistor = persistStore(store);
+const store = createStore(reducers, composeWithDevTools());
 
 export default store;
